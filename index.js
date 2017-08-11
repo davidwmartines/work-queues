f1268b54023472a8ce4ecd02a12891d5aedecd62
davidwmartines <david.martines@businesssoftwareengineering.com>
2017-08-11 11:15:02 -0500

    Add status route.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 app.set('port', (process.env.PORT || 3001));
 
+app.get('/api/status', (req, res) => {
+  res.json({
+    status: 'online'
+  });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 });
